10bd6806e2aa551900b4aa9cbb041802de39026c
pagidas <pagakrivos@gmail.com>
2022-06-26 11:11:11 +0100

    Pushed for parametrized tests
    As our test cases test the capability in the same way, we parameterized
    the test, leaving variants defined out of the test.

--- src/test/kotlin/org/example/checkout/CheckoutTest.kt
+++ src/test/kotlin/org/example/checkout/CheckoutTest.kt
@@ -2,32 +2,53 @@ package org.example.checkout
 
 import org.example.checkout.Checkout.Item
 import org.example.checkout.Checkout.SKU
-import org.junit.jupiter.api.Assertions.*
-import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CheckoutTest {
 
     val checkout: Checkout = checkoutLogic
 
-    @Test
-    fun `total price for no items`() {
-        assertEquals(0, checkout(emptyList()))
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("provideItemsAndExpectedTotal")
+    fun `checkout provides correct total price`(description: String, items: Items, expectedTotal: Long) {
+        assertEquals(expectedTotal, checkout(items))
     }
 
-    @Test
-    fun `total price for a single A`() {
-        val items = listOf(Item(SKU("A"), 50))
-        assertEquals(50, checkout(items))
-    }
-
-    @Test
-    fun `total price for multiple A`() {
-        val items = listOf(
-            Item(SKU("A"), 50),
-            Item(SKU("A"), 50),
-            Item(SKU("A"), 50)
+    private fun provideItemsAndExpectedTotal(): Stream<Arguments> =
+        Stream.of(
+            `no items`(),
+            `a single A`(),
+            `multiple A`()
         )
-        assertEquals(150, checkout(items))
-    }
+
+    private fun `no items`(): Arguments = Arguments.of(
+        "no items",
+        emptyList<Item>(),
+        0
+    )
+
+    private fun `a single A`(): Arguments = Arguments.of(
+        "a single A",
+        listOf(
+            Item(SKU("A"), 50)
+        ),
+        50
+    )
+
+    private fun `multiple A`(): Arguments = Arguments.of(
+        "multiple A",
+        listOf(
+            Item(SKU("A"), 50),
+            Item(SKU("A"), 50),
+            Item(SKU("A"), 50),
+        ),
+        150
+    )
 }
 
